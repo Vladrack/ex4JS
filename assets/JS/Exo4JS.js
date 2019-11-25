88d10c192b1de26820fbec7c980bca58bd916fb5
Vladrack <frg.cedric@gmail.com>
2019-11-25 14:21:39 +0100

    ex4jS

---
+++ assets/JS/Exo4JS.js
@@ -0,0 +1,6 @@
+function formByClic() {
+    var lastName = document.getElementById('pseudo').value;
+    var firstName = document.getElementById('prenom').value;
+    var city = document.getElementById('Ville').value;
+    alert('Nom : '+lastName+'\n'+'\n' + 'Prenom : ' +firstName + '\n'+'\n' + 'Ville : '+ city);
+}
